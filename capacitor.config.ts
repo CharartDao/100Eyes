8eb4db2fdbcdc99162d2ae4af6f2a1c34db7cd8c
ChenpoHU <maodauibe@gmail.com>
2023-02-12 16:05:06 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: '100Eyes',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
